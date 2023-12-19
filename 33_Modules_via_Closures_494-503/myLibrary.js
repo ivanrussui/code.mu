9a4cc83d41c0cd67286663735d17002c6917ed25
ivanrussui <fenomen_ny@mail.ru>
2023-12-19 13:48:46 +0300

    503 myLibrary find, filter, every, some

--- 33_Modules_via_Closures_494-503/myLibrary.js
+++ 33_Modules_via_Closures_494-503/myLibrary.js
@@ -2,7 +2,7 @@
 // важно, я тут не открываю исходный код underscore и переписываю.
 // я обращаюсь к библиотеке и пытаюсь реализовать логику так, чтобы на выходе получать те же данные
 
-import _, {each, first, map, reduce, reduceRight} from "./underscore-esm.js";
+// import _, {each, first, map, reduce, reduceRight} from "./underscore-esm.js";
 
 ;(function () {
     function each(list, callBack) {
@@ -53,8 +53,64 @@ import _, {each, first, map, reduce, reduceRight} from "./underscore-esm.js";
         return memo;
     }
 
-    // вспомогательные функции
+    function find(list, callBack, predicate) {
+        for (let el of list) {
+            if (callBack) {
+                predicate = callBack(el);
+                if (predicate) {
+                    return el;
+                }
+            } else if (el) {
+                return el;
+            }
+        }
+    }
 
+    function filter(list, callBack, predicate) {
+        let newArr = [];
+        for (let el of list) {
+            // думал тут юзать функцию callBackForArray, но она тогда получается очень умной
+            // наверное callBackForArray надо переписать, чтобы была проще
+            if (callBack) {
+                predicate = callBack(el);
+                if (predicate) {
+                    newArr.push(el);
+                }
+            } else {
+                return list;
+            }
+        }
+        return newArr;
+    }
+
+    function every(list, callBack, predicate) {
+        predicate = true;
+        for (let el of list) {
+            if (callBack) {
+                predicate = callBack(el);
+            }
+            if (!el) {
+                return predicate = false;
+            }
+        }
+        return predicate;
+    }
+
+    function some(list, callBack, predicate) {
+        predicate = false;
+        for (let el of list) {
+            if (callBack) {
+                if (callBack(el)) {
+                    predicate = true;
+                }
+            } else if (el) {
+                predicate = true;
+            }
+        }
+        return predicate;
+    }
+
+    // вспомогательные функции
     function callBackForArray(list, callBack, newArr) {
         for (let i = 0; i < list.length; i++) {
             let logic = callBack(list[i], i, list);
@@ -65,24 +121,50 @@ import _, {each, first, map, reduce, reduceRight} from "./underscore-esm.js";
     function callBackForObject(list, callBack, newArr) {
         let entries = Object.entries(list);
 
-        // for i
-        for (let i = 0; i < entries.length; i++) {
+        for (let i = 0; i < entries.length; i++) {  // for i
             let logic = callBack(entries[i][1], entries[i][0], list);
             newArr ? newArr.push(logic) : logic;
         }
 
-        // for of
-        // for (let el in entries) {
+        // for (let el in entries) { // for of
         //     let logic = callBack(entries[el][1], entries[el][0], list);
         //     newArr ? newArr.push(logic) : logic;
         // }
     }
 
-    window.myLibrary = {each, map, first, reduce, reduceRight};
+    window.myLibrary = {each, map, first, reduce, reduceRight, find, filter, every, some};
 })();
-//
+
+// console.log(_.some([null, 0, 'yes', false]));
+// console.log(myLibrary.some([null, 0, 'yes', false]));
+// console.log(_.some([1, 21, 3, 5], function (num) {
+//     return num % 2 === 0;
+// }));
+// console.log(myLibrary.some([1, 21, 3, 5], function (num) {
+//     return num % 2 === 0;
+// }));
+
+// console.log(_.every([true, 'yes', 2, '']));
+// console.log(myLibrary.every([true, 'yes', 2, '']));
+// console.log(_.every([2, 4, 5], function(num) { return num % 2 === 0; }));
+// console.log(myLibrary.every([2, 4, 5], function(num) { return num % 2 === 0; }));
+
+// console.log(_.filter([1, 2, 3, 4, 5, 6], function(num){ return num + 2 === 6; }));
+// console.log(myLibrary.filter([1, 2, 3, 4, 5, 6], function(num){ return num + 2 === 6; }));
+// console.log(_.filter([1, 2, 3, 4, 5, 6]));
+// console.log(myLibrary.filter([1, 2, 3, 4, 5, 6]));
+
+// console.log(_.find([null, 2, 3, 4, 5, 6]));
+// console.log(myLibrary.find([null, 2, 3, 4, 5, 6]));
+// console.log(_.find([1, 2, 3, 4, 5, 6], function (num) {
+//     return num + 3 === 7 ;
+// }));
+// console.log(myLibrary.find([1, 2, 3, 4, 5, 6], function (num) {
+//     return num + 3 === 7 ;
+// }));
+
 // let list = [[0, 1], [2, 3], [4, 5]];
-// let flat = reduceRight(list, function (a, b) {
+// let flat = _.reduceRight(list, function (a, b) {
 //     return a.concat(b);
 // }, []);
 // console.log(flat);
@@ -92,7 +174,7 @@ import _, {each, first, map, reduce, reduceRight} from "./underscore-esm.js";
 // }, []);
 // console.log(flat2);
 
-// let sum = reduce([[1, 2], [3, 4]], function (memo, num) {
+// let sum = _.reduce([[1, 2], [3, 4]], function (memo, num) {
 //     return memo + num;
 // }, 10);
 // console.log(sum);
@@ -102,14 +184,14 @@ import _, {each, first, map, reduce, reduceRight} from "./underscore-esm.js";
 // }, 10);
 // console.log(sum2);
 
-// console.log(map([[1, 2], [3, 4]], first));
-// // // тут еще не реализовал внтури map first
+// console.log(_.map([[1, 2], [3, 4]], _.first));
+// тут еще не реализовал внтури map first
 // тут нужно копать map если ты сразу в first передашь такую структуру то в underscore тож не робит
 // console.log(myLibrary.map([[1, 2], [3, 4]], myLibrary.first));
 //
-// console.log(first([5, 4, 3, 2, 1], 3));
+// console.log(_.first([5, 4, 3, 2, 1], 3));
 // console.log(myLibrary.first([5, 4, 3, 2, 1], 3));
-// console.log(first([5, 4, 3, 2, 1]));
+// console.log(_.first([5, 4, 3, 2, 1]));
 // console.log(myLibrary.first([5, 4, 3, 2, 1]));
 
 
