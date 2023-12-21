4b06ee9389d663806ba0cd001df3f1d117af3fab
Ivan <fenomen_ny@mail.ru>
2023-12-21 13:55:46 +0300

    1 1-4 update functions
    1 update functions

--- 02_Practice_Textbook/01_Practice_JavaScript/script.js
+++ 02_Practice_Textbook/01_Practice_JavaScript/script.js
@@ -4,12 +4,20 @@
 
 // 1 Калькуляторы фигур / Калькуляторы фигур на JavaScript
 
-// функция переиспользуется в нескольких калькуляторах
-function checkIsNaN(parameterOne, parameterTwo, clearValueAndText) {
-    if (isNaN(parameterOne) || isNaN(parameterTwo)) {
+// функции переиспользуется в калькуляторах
+
+function checkIsNaN(parameterOne, parameterTwo, parameterThree, clearValueAndText) {
+    if (isNaN(parameterOne) || isNaN(parameterTwo) || isNaN(parameterThree)) {
         alert('Вы ввели некорректное значение');
         clearValueAndText();
     }
+}
+
+function clearValueAndText(parameterOne, parameterTwo, parameterThree, parameterFour) {
+    parameterOne.value = '';
+    parameterTwo && (parameterTwo.value = '');
+    parameterThree && (parameterThree.value = '');
+    parameterFour.textContent = '';
 }
 
 
@@ -24,24 +32,16 @@ function checkIsNaN(parameterOne, parameterTwo, clearValueAndText) {
     btn.addEventListener('click', function () {
         if (side.value <= 0) {
             alert('Введите число больше 0');
-            clearValueAndText();
+            clearValueAndText(side, null, null, paragraph);
         } else {
             let area = side.value * side.value;  // площадь
             let perimeter = side.value * 4;  // периметр
             paragraph.textContent = `Площадь квадрата ${area} периметр квадрата ${perimeter}`;
-            // if (isNaN(area) || isNaN(perimeter)) {
-            //     alert('Вы ввели некорректное значение');
-            //     clearValueAndText();
-            // }
-            checkIsNaN(area, perimeter, clearValueAndText);
+            checkIsNaN(area, perimeter, null, function () {
+                clearValueAndText(side, null, null, paragraph);
+            });
         }
     });
-
-    // вспомогательная функция
-    function clearValueAndText() {
-        side.value = '';
-        paragraph.textContent = '';
-    }
 })('#square');
 
 
@@ -57,24 +57,19 @@ function checkIsNaN(parameterOne, parameterTwo, clearValueAndText) {
     btn.addEventListener('click', function () {
         if (length.value <= 0 || width.value <= 0) {
             alert('Введите числа больше 0');
-            clearValueAndText();
+            clearValueAndText(length, width, null, paragraph);
         } else if (length.value === width.value) {
             alert('Длина и ширина не могут быть равны у прямоугольника');
-            clearValueAndText();
+            clearValueAndText(length, width, null, paragraph);
         } else {
             let area = length.value * width.value;  // площадь
             let perimeter = 2 * (length.value + width.value);  // периметр
             paragraph.textContent = `Площадь прямоугольника ${area} периметр прямоугольника ${perimeter}`;
-            checkIsNaN(area, perimeter, clearValueAndText);
+            checkIsNaN(area, perimeter, null, function () {
+                clearValueAndText(length, width, null, paragraph);
+            });
         }
     });
-
-    // вспомогательная функция
-    function clearValueAndText() {
-        length.value = '';
-        width.value = '';
-        paragraph.textContent = '';
-    }
 })('#rectangle');
 
 
@@ -91,33 +86,33 @@ function checkIsNaN(parameterOne, parameterTwo, clearValueAndText) {
         let text = parent.querySelector('label[for="circleSegment"]');
         if (select.value === 'one') {
             text.textContent = 'Введите диаметр круга';
+            paragraph.textContent = '';
         } else {
             text.textContent = 'Введите радиус круга';
+            paragraph.textContent = '';
         }
     });
 
     btn.addEventListener('click', function () {
         if (segment.value <= 0) {
             alert('Введите число больше 0');
-            clearValueAndText();
+            clearValueAndText(segment, null, null, paragraph);
         } else if (select.value === 'one') {
             let area = segment.value ** 2 / 4 * 3.14; // площадь через диаметр
             let circumference = 3.14 * segment.value; // длина окружности через диаметр
             paragraph.textContent = `Площадь круга ${area} длина окружности ${circumference}`;
-            checkIsNaN(area, circumference, clearValueAndText);
+            checkIsNaN(area, circumference, null, function () {
+                clearValueAndText(segment, null, null, paragraph);
+            });
         } else {
             let area = 3.14 * segment.value ** 2; // площадь через радиус
             let circumference = 3.14 * segment.value * 2; // длина окружности через радиус
             paragraph.textContent = `Площадь круга ${area} длина окружности ${circumference}`;
-            checkIsNaN(area, circumference, clearValueAndText);
+            checkIsNaN(area, circumference, null, function () {
+                return clearValueAndText(segment, null, null, paragraph);
+            });
         }
     });
-
-    // вспомогательная функция
-    function clearValueAndText() {
-        segment.value = '';
-        paragraph.textContent = '';
-    }
 })('#circle');
 
 
@@ -125,26 +120,55 @@ function checkIsNaN(parameterOne, parameterTwo, clearValueAndText) {
 
 ;(function (root) {
     let parent = document.querySelector(root);
+    let sideOne = parent.querySelector('#triangleSideOne');
+    let sideTwo = parent.querySelector('#triangleSideTwo');
+    let sideThree = parent.querySelector('#triangleSideThree');
     let base = parent.querySelector('#triangleBase');
     let height = parent.querySelector('#triangleHeight');
+    let select = parent.querySelector('#triangleSelect');
+    let threeSides = parent.querySelector('#threeSides');
+    let baseAndHeight = document.querySelector('#baseAndHeight');
     let btn = parent.querySelector('button');
     let paragraph = parent.querySelector('p');
+    threeSides.classList.add('active');
 
-    btn.addEventListener('click', function () {
-        if (base.value <= 0 || height.value <= 0) {
-            alert('Введите числа больше 0');
-            clearValueAndText();
+    select.addEventListener('change', function () {
+        if (select.value === 'one') {
+            threeSides.classList.toggle('active');
+            baseAndHeight.classList.toggle('active');
+            paragraph.textContent = '';
         } else {
-            let area = 0.5 * base.value * height.value;  // площадь
-            paragraph.textContent = `Площадь треугольника ${area}`;
-            checkIsNaN(area, null, clearValueAndText); // передаю null 2м параметром
+            threeSides.classList.toggle('active');
+            baseAndHeight.classList.toggle('active');
+            paragraph.textContent = '';
         }
     });
 
-    // вспомогательная функция
-    function clearValueAndText() {
-        base.value = '';
-        height.value = '';
-        paragraph.textContent = '';
-    }
+    btn.addEventListener('click', function () {
+        if (select.value === 'one') {
+            if (sideOne.value <= 0 || sideTwo.value <= 0 || sideThree.value <= 0) {
+                alert('Введите числа больше 0');
+                clearValueAndText(sideOne, sideTwo, sideThree, paragraph);
+            } else {
+                let halfPerimeter = (+sideOne.value + +sideTwo.value + +sideThree.value) / 2; // полупериметр
+                let area = Math.sqrt(halfPerimeter * (halfPerimeter - sideOne.value) * (halfPerimeter - sideTwo.value) * (halfPerimeter - sideThree.value));
+                paragraph.textContent = `Площадь треугольника ${area}`;
+                checkIsNaN(area, null, null, function () {
+                    return clearValueAndText(sideOne, sideTwo, sideThree, paragraph);
+                });
+            }
+        } else {
+            if (base.value <= 0 || height.value <= 0) {
+                alert('Введите числа больше 0');
+                clearValueAndText(base, height, null, paragraph);
+            } else {
+                let area = 0.5 * base.value * height.value;  // площадь
+                paragraph.textContent = `Площадь треугольника ${area}`;
+                checkIsNaN(area, null, null, function () {
+                    return clearValueAndText(base, height, null, paragraph);
+                });
+            }
+        }
+    });
 })('#triangle');
+
