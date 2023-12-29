c71c97fafe0ee21b18dbccef41cad7058b27676e
ivanrussui <fenomen_ny@mail.ru>
2023-12-29 22:33:36 +0300

    04_Guess_The_Cell_Game 2 complete

--- 02_Practice_Textbook/01_Practice_JavaScript/04_Guess_The_Cell_Game/script.js
+++ 02_Practice_Textbook/01_Practice_JavaScript/04_Guess_The_Cell_Game/script.js
@@ -10,19 +10,19 @@
 
 // 1 Реализуйте описанную игру.
 
-// let body = document.querySelector('body');
 let title = document.querySelector('h1');
 let paragraph = document.querySelector('p');
 let table = document.createElement('table');
 
-// body.insertBefore(table, paragraph);
-title.parentElement.append(table)
+title.parentElement.insertBefore(table, paragraph);
 
 let k = 1;  // счетчик
 
 for (let i = 0; i < 10; i++) {
     let tr = document.createElement('tr');
     table.append(tr);
+    table.classList.add('disable');
+
     for (let j = 0; j < 10; j++) {
         let td = document.createElement('td');
         tr.append(td);
@@ -46,74 +46,83 @@ function getRandomsTds(tds) {
         randoms.push(Math.floor(Math.random() * (+tds[99].textContent - +tds[0].textContent + 1)) + +tds[0].textContent);
     }
 
-    paragraph.textContent = `Отгадайте ${randoms.length} случайных ячеек`;
-    return randoms;
+    let uniqueArr = getUniqueArr(randoms);
+    console.log(uniqueArr.length);
+    if (uniqueArr.length < 10) {
+        console.log('recursion');
+        return getRandomsTds(tds);
+    }
+
+    paragraph.textContent = `Отгадайте ${uniqueArr.length} случайных ячеек`;
+    return uniqueArr;
 }
+
+let getUniqueArr = randoms => randoms.filter((item, index) => randoms.indexOf(item) === index);
 
 let res = getRandomsTds(tds);
 console.log('res', res);
 
-for (let i = 0; i < tds.length; i++) {
-    tds[i].addEventListener('click', function (e) {
+function clickOnCells() {
+    for (let i = 0; i < tds.length; i++) {
+        tds[i].addEventListener('click', function (e) {
+            let deleteEL = res.indexOf(+e.target.textContent);
 
-        let deleteEL = res.indexOf(+e.target.textContent);
+            if (deleteEL >= 0) {
+                res.splice(deleteEL, 1);
+                paragraph.textContent = `Осталось отгадать ${res.length}`;
+                tds[i].classList.add('right');
+            } else {
+                tds[i].classList.add('wrong');
+            }
 
-        if (deleteEL >= 0) {
-            res.splice(deleteEL, 1);
-            paragraph.textContent = `Осталось отгадать ${res.length}`;
-        }
+            if (res.length === 0) {
+                paragraph.textContent = 'Вы выйграли!';
+                paragraph.classList.add('green');
+                clearInterval(timerId);
+                timer.textContent = `Поздравляем у вас оставалось ${seconds} секунд!`;
+                timer.classList.add('indigo');
+                start.disabled = true;
+                stop.disabled = true;
+            }
 
-        if (res.length === 0) {
-            paragraph.textContent = 'Вы выйграли!';
-            paragraph.classList.add('green');
-            clearInterval(timerId);
-            timer.textContent = `У вас в запасе ${seconds} секунд!`;
-            timer.classList.add('indigo');
-        }
-
-        console.log('res', res);
-    });
+            console.log('res', res);
+        });
+    }
 }
+
+clickOnCells();
 
 
 // 2 Модифицируйте предыдущую задачу, добавив таймер обратного отсчета.
 // Если игрок не успеет угадать числа за отведенное время - он проиграл.
 
-let timer = document.querySelector('h4');
-let btn = document.querySelector('button');
-// let time = 11000;
-let time = 300000;
+let timer = document.querySelector('#timer');
+let start = document.querySelector('#start');
+let stop = document.querySelector('#stop');
 
-let timerId
-let timerId2
-
-function runSetTimeout() {
-    timerId2 = setTimeout(() => {
-        paragraph.textContent = 'Вы проиграли!';
-        paragraph.classList.add('red');
-    }, time);
-}
-
-runSetTimeout();
-// console.log(timerId);
-
+let time = 30000;
 let seconds = time / 1000;
-function runSetInterval() {
+let timerId;
+
+function startTimer() {
     timerId = setInterval(() => {
-        // console.log(seconds--);
         timer.textContent = `У вас осталось ${--seconds} секунд`;
+
         if (seconds <= 0) {
             clearInterval(timerId);
+            paragraph.textContent = 'Вы проиграли!';
+            paragraph.classList.add('red');
         }
     }, 1000);
-}
-runSetInterval();
 
-// btn.addEventListener('click', function func() {
-//
-//     clearTimeout(timerId2)
-//     clearInterval(timerId)
-//     runSetTimeout();
-//     runSetInterval();
-//     this.removeEventListener('click', func);
-// });
+    table.classList.remove('disable');
+    start.removeEventListener('click', startTimer);
+}
+
+start.addEventListener('click', startTimer);
+
+stop.addEventListener('click', function () {
+    clearInterval(timerId);
+    table.classList.add('disable');
+    start.addEventListener('click', startTimer);
+});
