fb5d47787bf5bb3d7ce1a693e087489c90936b81
Ivan <fenomen_ny@mail.ru>
2024-10-21 20:49:43 +0300

    Хранилище 86 - 91

--- 03_Higher_Textbook/10_Storage/script.js
+++ 03_Higher_Textbook/10_Storage/script.js
@@ -157,14 +157,171 @@
 // 3 Дан инпут. По потери фокуса в этом инпуте сохраните его значение в локальное хранилище.
 // При следующем заходе пользователя на страницу установите в инпуте сохраненный ранее текст.
 
-const input = document.querySelector('input');
+// const input = document.querySelector('input');
+//
+// const savedValue = localStorage.getItem('input');
+//
+// if (savedValue) {
+//     input.value = savedValue;
+// }
+//
+// input.addEventListener('blur', function () {
+//     localStorage.setItem('input', this.value);
+// });
 
-const savedValue = localStorage.getItem('input');
 
-if (savedValue) {
-    input.value = savedValue;
-}
+// 86) Перезапись данных / Перезапись данных в localStorage в JavaScript
+//
+// Можно перезаписывать данные, хранящиеся в хранилище под определенным ключом. Давайте попробуем. Запишем какие-нибудь данные:
+// localStorage.setItem('key', '1');
+// let value1 = localStorage.getItem('key');
+//
+// console.log(value1); // выведет '1'
+//
+// А теперь перезапишем эти данные::
+// localStorage.setItem('key', '2');
+// let value2 = localStorage.getItem('key');
+//
+// console.log(value2); // выведет '2'
 
-input.addEventListener('blur', function () {
-    localStorage.setItem('input', this.value);
+
+// 1 Пользователь заходит на сайт, затем обновляет страницу, затем еще раз обновляет и так далее.
+// Сделайте счетчик обновления страницы. Каждый раз при обновлении выводите значение счетчика на экран.
+//
+// const div = document.querySelector('div');
+// let count = localStorage.getItem('count');
+//
+// if (!count) {
+//     count = 0;
+//     localStorage.setItem('count', count);
+// } else {
+//     count++;
+//     localStorage.setItem('count', count);
+// }
+// div.textContent = `Счетчик: ${count}`;
+
+
+// 87) Удаление данных / Удаление данных в localStorage в JavaScript
+//
+// С помощью метода removeItem можно удалять данные и связанный с ними ключ.
+// Давайте для примера удалим содержимое какого-нибудь ключа:
+// localStorage.removeItem('key');
+//
+// Проверим теперь наличие данных:
+//
+// let value = localStorage.getItem('key');
+// console.log(value); // выведет null
+
+
+// 1 Сделайте счетчик обновления страницы. Пусть счетчик обнуляется после того, как дойдет до 10.
+//
+// const div = document.querySelector('div');
+// let count = localStorage.getItem('count');
+// const countMax = '10';
+//
+// const setItemToLocaleStorage = (key, value) => localStorage.setItem(key, value);
+//
+// if (!count) {
+//     count = 0;
+//     setItemToLocaleStorage('count', count);
+// } else if (count === countMax) {
+//     localStorage.removeItem('count');
+//     setItemToLocaleStorage('count', '0');
+// } else {
+//     count++;
+//     setItemToLocaleStorage('count', count);
+// }
+//
+// div.textContent = `Счетчик: ${count === countMax ? '0': count}`;
+
+
+// 88) Очистка хранилища / Очистка хранилища в localStorage в JavaScript
+//
+// С помощью метода clear можно очистить все хранилище. Смотрите пример:
+// localStorage.clear();
+
+
+// 1 Очистите хранилище. Проверьте, что все ранее установленные вами ключи пропали.
+//
+// localStorage.clear();
+
+
+// 89) Количество записей / Количество записей в localStorage в JavaScript
+//
+// С помощью свойства length можно узнать количество записей в локальном хранилище:
+
+
+// 1 По нажатию на кнопку выведите количество записей в локальном хранилище.
+//
+// const div = document.querySelector('div');
+// const button = document.querySelector('button');
+//
+// button.addEventListener('click', () => {
+//     div.textContent = `Количество записей в локальном хранилище: ${localStorage.length}`;
+// });
+
+
+// 2 По нажатию на кнопку узнайте количество записей в локальном хранилище и, если их больше 10, очистите хранилище.
+//
+// const div = document.querySelector('div');
+// const button = document.querySelector('button');
+//
+// button.addEventListener('click', () => {
+//     div.textContent = `Количество записей в локальном хранилище: ${localStorage.length}`;
+//     localStorage.length > 10 && localStorage.clear();
+// });
+
+
+// 90) Получение ключа по номеру / Получение ключа по номеру в localStorage в JavaScript
+//
+// Каждая запись в локальном хранилище имеет свой номер. По номеру можно получить ключ этой записи:
+// let key = localStorage.key(5);
+// console.log(key);
+//
+// Зная ключ, можно получить значение этой записи:
+// let key = localStorage.key(0);
+// let val = localStorage.getItem(key);
+// console.log(val);
+
+// 1 Получите значение записей с различными номерами.
+// localStorage.setItem('key1', 'One');
+// localStorage.setItem('key2', 'Two');
+// localStorage.setItem('key3', 'Three');
+//
+// const key1 = localStorage.key(0);
+// const key2 = localStorage.key(1);
+// const key3 = localStorage.key(2);
+//
+// const value1 = localStorage.getItem(key1)
+// const value2 = localStorage.getItem(key2)
+// const value3 = localStorage.getItem(key3)
+//
+// console.log(key1, value1);
+// console.log(key2, value2);
+// console.log(key3, value3);
+
+
+// 91) Перебор хранилища по индексам / Перебор хранилища по индексам в JavaScript
+//
+// Объект localStorage не является итерируемым. Тем не менее в нем есть индексы и длина.
+//
+// Это значит, что можно выполнить перебор записей через обычный цикл for со счетчиком:
+// for (let i = 0; i < localStorage.length; i++) {
+// 	let key = localStorage.key(i);
+// 	let val = localStorage.getItem(key);
+//
+// 	console.log(val);
+// }
+
+// 1 По нажатию на кнопку выведите циклом все записи из локального хранилища.
+const btn = document.querySelector('button');
+const div = document.querySelector('div');
+
+btn.addEventListener('click', () => {
+    div.innerHTML = '';
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        const value = localStorage.getItem(key);
+        div.innerHTML += `<p>${key}: ${value}</p>`;
+    }
 });
